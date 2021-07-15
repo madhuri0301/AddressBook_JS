83049fb6a0f217536f36fbc7f644d140ffcbc826
Madhuri <harshupatil0301@gmail>
2021-07-15 15:02:20 +0530

    [Madhuri]Add : Ability To Ensure Valid Contacts Using Regex

--- addressBook.js
+++ addressBook.js
@@ -20,12 +20,130 @@ class Contact{
         this.phone = params[6];
         this.email = params[7];
     }
+
+
+    get firstName()
+    {
+        return this._firstName;
+    }
+
+    set firstName(firstName)
+    {
+        let firstNameregex = RegExp('^[A-Z][a-z]{2,}$');
+        if(firstNameregex.test(firstName))
+        this._firstName=firstName;
+        
+        else
+        throw 'First Name Invalid'
+    }
+
+    get lastName()
+    {
+        return this._lastName;
+    }
+
+    set lastName(lastName)
+    {
+        let lastNameregex= RegExp('^[A-Z][a-z]{2,}$');
+        if(lastNameregex.test(lastName))
+        this._lastName=lastName;
+
+        else
+        throw 'Last Name Invalid'
+    }
+
+    set address(address)
+    {
+        let addressRegex = RegExp('^[A-Za-z\\s0-9]{4,}$');
+        if(addressRegex.test(address))
+        this._address = address;
+        else
+        throw 'Address Invalid'
+    }
+
+    get city()
+    {
+        return this._city;
+    }
+
+    set city(city)
+    {
+        let cityRegex = RegExp('^[A-Za-z\\s]{4,}$');
+        if(cityRegex.test(city))
+        this._city = city;
+        else
+        throw 'City Invalid'
+    }
+
+    get state()
+    {
+        return this._state;
+    }
+
+    set state(state)
+    {
+        let stateRegex = RegExp('^[A-Za-z\\s]{4,}$')
+        if(stateRegex.test(state))
+        this._state = state;
+        else
+        throw 'State Invalid'
+    }
+
+    get zip()
+    {
+        return this._zip;
+    }
+
+    set zip(zip)
+    {
+        let zipRegex = RegExp('^[0-9]{3}[\\s]*[0-9]{2,}$');
+        if(zipRegex.test(zip))
+        this._zip = zip;
+        else
+        throw 'Zip Invalid'
+    }
+
+    get phone()
+    {
+        return this._phone;
+    }
+
+    set phone(phone)
+    {
+        let phoneRegex = RegExp('^[1-9][0-9]{9}$');
+        if(phoneRegex.test(phone))
+        this._phone = phone;
+        else
+        throw 'Phone Invalid'
+    }
+
+    get email()
+    {
+        return this._email;
+    }
+
+    set email(email)
+    {
+        let emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z_+.-]*[a-zA-Z]+@[a-zA-Z]+[.][a-zA-z]{2,}$');
+        if(emailRegex.test(email))
+        this._email = email;
+        else
+        throw 'Email Incorrect';
+    }
+
     toString()
     {
         return "FirstName = "+this.firstName+", LastName = "+this.lastName+", Address = "+this.address+
                 ", City = "+this.city+", State = "+this.state+", Zip = "+this.zip+", Phone = "+this.phone+", Email = "+this.email;
     }
-
 }
-let contact = new Contact("Madhuri","Patil","101,Gujar Lane","Shahada","Maharashtra",425409,8976564567,"harshupatil0301@gmail.com");
-console.log(contact.toString());
+
+    try
+    {
+        let contact = new Contact("Madhuri","Patil","Gujar Gully","Shahada","Maharashtra",425409,7789765456,"harshu03@gmail.com");
+        console.log(contact.toString());
+    }
+    catch(e)
+    {
+        console.error(e);
+    }
